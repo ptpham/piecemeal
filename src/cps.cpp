a83b16a17d911a8cdcfe5bc83c9ccec699e5a568
Peter Pham <ptr.pham@gmail.com>
2014-10-26 12:41:01 -0700

    Adding transitions per second to cps.

--- src/cps.cpp
+++ src/cps.cpp
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  size_t niterations = atoi(argv[2]);
+  size_t niterations = atoi(argv[2]), transitions = 0;
   game::machine<> machine(ioaux::read_all(argv[1]));
 
   auto begin = clock();
@@ -27,10 +27,12 @@ int main(int argc, char* argv[]) {
     machine.restart();
     while (!machine.terminal()) {
       machine.move(random_joint_move(machine.moves()));
+      transitions++;
     }
   }
   auto seconds = (double)(clock() - begin)/CLOCKS_PER_SEC;
   cout << "Charges per second: " << (double)niterations/seconds << endl;;
+  cout << "Transitions per second: " << (double)transitions/seconds << endl;;
 
   return 0;
 }
